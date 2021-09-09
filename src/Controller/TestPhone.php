3e477f3001b334683e62c2887b384866212b8e01
MoRzE <alexvin_noyz@mail.ru>
2021-09-09 10:38:41 +0300

    0909_1

---
+++ src/Controller/TestPhone.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use libphonenumber\PhoneNumberUtil;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestPhone
+{
+    /**
+     * @Route("/inphone", methods={"POST"})
+     */
+    public function recievePhone(Request $request): Response
+    {
+        $phone = $request->request->get("phone");
+        return new Response("");
+    }
+
+    /**
+     * @Route("/test")
+     */
+    public function test()
+    {
+        $testNum = "89263847591";
+        $checkPhone = PhoneNumberUtil::getInstance();
+        $testNum = $checkPhone->parse($testNum, "RU");
+        $isValid = $checkPhone->isValidNumber($testNum);
+        dd($isValid);
+    }
+}
