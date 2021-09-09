60da55fa469ceac26a59fca34152e7b336733839
MoRzE <alexvin_noyz@mail.ru>
2021-09-09 14:15:25 +0300

    0909_1

--- src/Controller/TestPhone.php
+++ src/Controller/TestPhone.php
@@ -8,6 +8,7 @@ use libphonenumber\PhoneNumberUtil;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function MongoDB\BSON\fromJSON;
 
 class TestPhone
 {
@@ -18,7 +19,7 @@ class TestPhone
     {
 
         $phone = $request->request->get("phone");
-        GeneralFuncs::createWriteFile(dirname(__FILE__)."/log.txt", "a+", serialize($_REQUEST)."\n".$phone."\n");
+        GeneralFuncs::createWriteFile(dirname(__FILE__)."/log.txt", "a+", serialize($request->request)."\n".$phone."\n");
         $checkPhone = PhoneNumberUtil::getInstance();
         try{
             $phone = $checkPhone->parse($phone, "RU");
